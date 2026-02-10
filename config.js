72f1c79d1f6a8acb463b215e2c7b28f58d743dc3
kvabishek2012-sudo <kvabishek2012@gmail.com>
2026-02-10 22:59:09 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // ============================================
-// 💝 CUSTOMIZE YOUR VALENTINE'S WEBSITE HERE 💝
+// 💝 IS THIS WHAT YOU WANTED? 💝
 // ============================================
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Delisha", "Anjitesh", "Mike"
-    valentineName: "Delisha",
+    valentineName: "Sharani",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -80,7 +80,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dpw8ll2ds/video/upload/v1770336511/Vellakkara_Durai_-_Ammadi_Un_Azhagu_Video_Vikram_Prabhu_D_mp3cut.net_mxaova.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
